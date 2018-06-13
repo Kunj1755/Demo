904cfbc6f420f0fb972457ff907f5812cadebb0a
kunj1755 <kunj1755@192.168.2.3>
2018-06-13 22:26:06 +0530

    Spring MVC app with java-based configuration

---
+++ src/main/java/com/demo/utils/AppConfig.java
@@ -0,0 +1,30 @@
+package com.demo.utils;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@EnableWebMvc
+@ComponentScan(basePackages="com.demo.controllers")
+@Configuration
+public class AppConfig extends WebMvcConfigurerAdapter{
+
+	@Bean
+	public InternalResourceViewResolver getIVR() {
+		System.out.println("Setting up view resolver");
+		InternalResourceViewResolver irvr = new InternalResourceViewResolver();
+		irvr.setPrefix("WEB-INF/jsp/");
+		irvr.setSuffix(".jsp");
+		return irvr;
+	}
+	
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		System.out.println("Setting up resource");
+		registry.addResourceHandler("/myResources/**").addResourceLocations("/resources");
+	}
+}
